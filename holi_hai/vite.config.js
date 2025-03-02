00327b25582c7c842c58c0a17892e8257ec32d0c
himanshusharma <hs0242262@gmail.com>
2025-03-02 19:40:52 +0530

    c3

--- holi_hai/vite.config.js
+++ holi_hai/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+// });
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  base: "/Holi_Special/", // Add your repository name here
+  plugins: [react(),
+    tailwindcss()],
 });
